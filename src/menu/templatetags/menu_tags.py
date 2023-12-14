ffca9f530e2bded749991f63ddf8f7905d68c484
Ekaterina <katerina.rubelle@gmail.com>
2023-12-14 14:41:52 +0600

    task-9: Implemented active menu item determining based on current URL

--- src/menu/templatetags/menu_tags.py
+++ src/menu/templatetags/menu_tags.py
@@ -1,13 +1,30 @@
+"""Module contains custom template tags."""
 from django import template
-from menu.models import Menu, MenuItem
-from menu.utils import get_all_menu_items, build_menu_tree
+
+from menu.utils import build_menu_tree, get_all_menu_items
 
 register = template.Library()
 
 
-@register.inclusion_tag('menu/menu_template.html')
-def draw_menu(menu_name):
+@register.inclusion_tag('menu/menu_template.html', takes_context=True)
+def draw_menu(context, menu_name):
+    """
+    Render a hierarchical menu based on the specified menu name.
+
+    Args:
+        context (dict): The template context, includes the current request.
+        menu_name (str): The name of the menu to be rendered.
+
+    Returns:
+        dict: A context dictionary containing the root menu items,
+        the complete menu tree, and the current URL.
+    """
+    request = context['request']
+    current_url = request.path
     all_items = get_all_menu_items(menu_name)
     menu_tree = build_menu_tree(all_items)
-    return {'menu_items': menu_tree.get(None), 'menu_tree': menu_tree}
-
+    return {
+        'menu_items_no_parent': menu_tree.get(None),
+        'menu_tree': menu_tree,
+        'current_url': current_url,
+    }
